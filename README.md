2664017101d8067d200674850b558d975f93972d
Alexis BERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2023-01-11 14:29:20 +0100

    Page artistes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=9708769&assignment_repo_type=AssignmentRepo)
-- NOM : 
-- PRÉNOM :
-- URL du site :
-- URL FIGMA :
+- NOM : BERNARD
+- PRÉNOM : Alexis
+- URL du site : http://leafs-artisans.alexisbernardev.fr/fr_html/
+- URL FIGMA : https://www.figma.com/file/XzroNhABe7iEdK8hRATsg2/SAE-105-Alexis-BERNARD--Leaf-Artisans?node-id=1349%3A924&t=DgViuFIQ5vFaylRS-1
+
+Préference utilisation appareil :  Iphone X
 
 # Structure de départ pour la SAE 105.
 
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+let images = ["../../public/img/artistes/william_morris.webp", "../../public/img/artistes/la_belle_iseult.webp","../../public/img/artistes/strawberry_thief.webp","../../public/img/artistes/peacock_and_dragon.webp"];
+let altText = ["portrait william morris", "oeuvre la belle iseult", "oeuvre strawberry thief","oeuvre peacock and dragon"]; 
+
+let images1 = ["../../public/img/artistes/eric_gills.webp", "../../public/img/artistes/mary_magdalen.webp","../../public/img/artistes/woodcut_showing_hammersmith.webp","../../public/img/artistes/self_portrait.webp"];
+let altText1 = ["portrait eric gills", "oeuvre lmary magdalen", "oeuvre woodcut showing hammersmith","self_portrait.webp"];  
+
+let images2 = ["../../public/img/artistes/gustav_stickley.webp", "../../public/img/artistes/oak_and_leather_armchair.webp","../../public/img/artistes/tree_of_life_pedestal.webp","../../public/img/artistes/Leather_Top_Table.webp"];
+let altText2 = ["portrait gustav stickley", "oeuvre oak and leather armchair", "oeuvre tree of life pedestal","oeuvre Leather Top Table."];  
+
+let images3 = ["../../public/img/artistes/charles_rohlfs.webp", "../../public/img/artistes/Desk Chair.webp","../../public/img/artistes/Carved_Oak_Barrel_Chair.webp","../../public/img/artistes/octagonal_cabinet.webp"];
+let altText3 = ["portrait charles rohlfs", "oeuvre desk chair", "oeuvre Carved Oak Barrel Chair","oeuvre octagonal cabinet"]; 
